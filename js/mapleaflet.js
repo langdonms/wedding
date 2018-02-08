bdbae6e90f2bee10c3803d883f6672f7237ce038
langdonms <langdon.sanders@gmail.com>
2018-02-07 22:58:27 -0500

    working with cards, improved map

--- js/mapleaflet.js
+++ js/mapleaflet.js
@@ -1,4 +1,8 @@
-    var map = L.map('map')
+    var map = L.map('map',
+        {
+        scrollWheelZoom:false
+        }
+      )
         .setView([40.1764, -83.1397], 15);
         
         L.tileLayer('https://api.mapbox.com/styles/v1/langdonms/cjddrtlfndyz22rkegavnupoi/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoibGFuZ2Rvbm1zIiwiYSI6ImNpZmQxdzE1dzRyd2RzbWx4bnp2Ym5maDcifQ.mrTKjuh7OcKwTDWc95OlZQ', {
@@ -12,3 +16,5 @@
         {title: "Ohio Wildlife Center"})
         .bindPopup('<strong>Ohio Wildlife Center</strong><br><a href="https://www.google.com.sg/maps/place/Ohio+Wildlife+Center/@40.1442272,-83.1831194,13z/data=!4m8!1m2!2m1!1sohio+wildlife+center,+powell+ohio!3m4!1s0x0:0x7de0bfc391441a5a!8m2!3d40.1763813!4d-83.139714">6131 Cook Rd, Powell, OH 43065</a>').openPopup()
         .addTo(map);
+        
+   
