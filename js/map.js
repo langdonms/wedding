fa38906f5cf3b61859cc4d5bd1584cb379afaa86
langdonms <langdon.sanders@gmail.com>
2018-02-07 20:28:41 -0500

    updated map style to use mapbox, changed name and updated the map JS to use leaflet, with also a MapBox place holder, added SVG flower design

--- js/map.js
+++ js/map.js
@@ -1,15 +1,10 @@
-    var map = L.map('mapid')
-        .setView([40.1764, -83.1397], 15);
-        
-        
-        L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            maxZoom: 18,
-            minZoom: 3,
-            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-        }).addTo(map);
-   
-   
-   var barnmarker = L.marker([40.1764, -83.1397],
-        {title: "Ohio Wildlife Center"})
-        .bindPopup('<strong>Ohio Wildlife Center</strong><br><a href="https://www.google.com.sg/maps/place/Ohio+Wildlife+Center/@40.1442272,-83.1831194,13z/data=!4m8!1m2!2m1!1sohio+wildlife+center,+powell+ohio!3m4!1s0x0:0x7de0bfc391441a5a!8m2!3d40.1763813!4d-83.139714">6131 Cook Rd, Powell, OH 43065</a>').openPopup()
-        .addTo(map);
+   mapboxgl.accessToken = 'pk.eyJ1IjoibGFuZ2Rvbm1zIiwiYSI6ImNpZmQxdzE1dzRyd2RzbWx4bnp2Ym5maDcifQ.mrTKjuh7OcKwTDWc95OlZQ';
+  var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v10'
+  });   
+    
+    
+    
+
+
---
+++ js/mapleaflet.js
@@ -0,0 +1,14 @@
+    var map = L.map('map')
+        .setView([40.1764, -83.1397], 15);
+        
+        L.tileLayer('https://api.mapbox.com/styles/v1/langdonms/cjddrtlfndyz22rkegavnupoi/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoibGFuZ2Rvbm1zIiwiYSI6ImNpZmQxdzE1dzRyd2RzbWx4bnp2Ym5maDcifQ.mrTKjuh7OcKwTDWc95OlZQ', {
+            maxZoom: 18,
+            minZoom: 3,
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        }).addTo(map);
+   
+   
+   var barnmarker = L.marker([40.1764, -83.1397],
+        {title: "Ohio Wildlife Center"})
+        .bindPopup('<strong>Ohio Wildlife Center</strong><br><a href="https://www.google.com.sg/maps/place/Ohio+Wildlife+Center/@40.1442272,-83.1831194,13z/data=!4m8!1m2!2m1!1sohio+wildlife+center,+powell+ohio!3m4!1s0x0:0x7de0bfc391441a5a!8m2!3d40.1763813!4d-83.139714">6131 Cook Rd, Powell, OH 43065</a>').openPopup()
+        .addTo(map);
