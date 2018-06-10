423a41dca0cf61410170116416a1e11a1fbd28ed
langdonms <langdon.sanders@gmail.com>
2018-06-10 15:12:35 -0400

    Added map in RSVP form area using leaflet

--- js/map.js
+++ js/map.js
@@ -1,10 +1,13 @@
    mapboxgl.accessToken = 'pk.eyJ1IjoibGFuZ2Rvbm1zIiwiYSI6ImNpZmQxdzE1dzRyd2RzbWx4bnp2Ym5maDcifQ.mrTKjuh7OcKwTDWc95OlZQ';
-  var map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v10'
-  });   
+var map = new mapboxgl.Map({
+    container: 'map', // container id
+    style: 'mapbox://styles/mapbox/streets-v9', // stylesheet location
+    center: [-74.50, 40], // starting position [lng, lat]
+    zoom: 9 // starting zoom
+});
     
     
     
+
 
 
