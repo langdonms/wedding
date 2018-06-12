cf5a48d0228f542f667e4c737956983c74957589
langdonms <langdon.sanders@gmail.com>
2018-06-11 22:27:44 -0400

    updated map, basic streets base, added markers

--- js/map.js
+++ js/map.js
@@ -1,13 +1,32 @@
-   mapboxgl.accessToken = 'pk.eyJ1IjoibGFuZ2Rvbm1zIiwiYSI6ImNpZmQxdzE1dzRyd2RzbWx4bnp2Ym5maDcifQ.mrTKjuh7OcKwTDWc95OlZQ';
+mapboxgl.accessToken = 'pk.eyJ1IjoibGFuZ2Rvbm1zIiwiYSI6ImNpZmQxdzE1dzRyd2RzbWx4bnp2Ym5maDcifQ.mrTKjuh7OcKwTDWc95OlZQ';
 var map = new mapboxgl.Map({
     container: 'map', // container id
     style: 'mapbox://styles/mapbox/streets-v9', // stylesheet location
-    center: [-74.50, 40], // starting position [lng, lat]
-    zoom: 9 // starting zoom
+    center: [-83.1397, 40.1764], // starting position [lng, lat]
+    zoom: 10 // starting zoom
 });
-    
-    
-    
 
 
-
+map.on("load", function () {
+  /* Image: An image is loaded and added to the map. */
+  map.loadImage("https://i.imgur.com/MK4NUzI.png", function(error, image) {
+      if (error) throw error;
+      map.addImage("custom-marker", image);
+      /* Style layer: A style layer ties together the source and image and specifies how they are displayed on the map. */
+      map.addLayer({
+        id: "markers",
+        type: "symbol",
+        /* Source: A data source specifies the geographic coordinate where the image marker gets placed. */
+        source: {
+          type: "geojson",
+          data: {
+            type: "FeatureCollection",
+            features:[{"type":"Feature","geometry":{"type":"Point","coordinates":
+            [-83.13829779624939,40.17670102282616]}}]}
+        },
+        layout: {
+          "icon-image": "custom-marker",
+        }
+      });
+    });
+});
--- js/mapleaflet.js
+++ js/mapleaflet.js
@@ -3,18 +3,31 @@
         scrollWheelZoom:false
         }
       )
-        .setView([40.1764, -83.1397], 15);
+        .setView([40.1764, -83.1397], 11);
         
-        L.tileLayer('https://api.mapbox.com/styles/v1/langdonms/cjhq6m9kq4rtu2st8ipatkg2q/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoibGFuZ2Rvbm1zIiwiYSI6ImNpZmQxdzE1dzRyd2RzbWx4bnp2Ym5maDcifQ.mrTKjuh7OcKwTDWc95OlZQ', {
+        L.tileLayer('https://api.mapbox.com/styles/v1/langdonms/cjib1sykx84rt2rmzlfibvur9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoibGFuZ2Rvbm1zIiwiYSI6ImNpZmQxdzE1dzRyd2RzbWx4bnp2Ym5maDcifQ.mrTKjuh7OcKwTDWc95OlZQ',
+
+        {
             maxZoom: 18,
             minZoom: 3,
-            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+            attribution: '© <a href="https://www.mapbox.com/about/maps/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> <strong><a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a></strong>'
         }).addTo(map);
-   
-   
-   var barnmarker = L.marker([40.1764, -83.1397],
-        {title: "Ohio Wildlife Center"})
-        .bindPopup('<strong>Ohio Wildlife Center</strong>').openPopup()
-        .addTo(map);
+
         
    
+   var barnmarker = L.marker([40.1764, -83.1397],
+   
+        {title: "Ohio Wildlife Center"})
+        .bindPopup('<strong>Venue</strong><br>Ohio Wildlife Center').openPopup()
+        .addTo(map);
+        
+   var hotelemb = L.marker([40.098346, -83.128843,],
+        {title: "Embassy Suites"})
+        .bindPopup('<strong>Embassy Suites</strong>').openPopup()
+        .addTo(map);
+        
+   var hotelhome2= L.marker([40.098464, -83.125232,],
+        {title: "Ohio Wildlife Center"})
+        .bindPopup('<strong>Home2 Suites').openPopup()
+        .addTo(map)
+        
