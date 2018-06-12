8aaa82eb39189c9cb5429c133f64689c67dff16a
langdonms <langdon.sanders@gmail.com>
2018-06-11 23:27:11 -0400

    updated map, some custom icons

--- js/mapleaflet.js
+++ js/mapleaflet.js
@@ -13,21 +13,33 @@
             attribution: '© <a href="https://www.mapbox.com/about/maps/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> <strong><a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a></strong>'
         }).addTo(map);
 
-        
-   
-   var barnmarker = L.marker([40.1764, -83.1397],
-   
-        {title: "Ohio Wildlife Center"})
+var hotelIcon = L.icon({
+    iconUrl: 'res/building-15-nob.png',
+    iconSize:     [26,26], // size of the icon
+    iconAnchor:   [13, 13], // point of the icon which will correspond to marker's location
+    popupAnchor:  [0, -13] // point from which the popup should open relative to the iconAnchor
+});        
+
+var heartIcon = L.icon({
+    iconUrl: 'res/heart-15.png',
+    iconSize:     [20,20], // size of the icon
+    iconAnchor:   [10, 4], // point of the icon which will correspond to marker's location
+});        
+
+var venuemarker = L.marker([40.1766,-83.1392517,],
+        {title: "Ohio Wildlife Center",
+        icon: heartIcon
+        }
+        )
         .bindPopup('<strong>Venue</strong><br>Ohio Wildlife Center').openPopup()
+        .addTo(map);        
+
+   var hotelemb = L.marker([40.098282,-83.1273043],
+        {title: "Embassy Suites",
+        icon: hotelIcon,
+        })
+        .bindPopup('<strong>Hotels</strong>').openPopup()
         .addTo(map);
-        
-   var hotelemb = L.marker([40.098346, -83.128843,],
-        {title: "Embassy Suites"})
-        .bindPopup('<strong>Embassy Suites</strong>').openPopup()
-        .addTo(map);
-        
-   var hotelhome2= L.marker([40.098464, -83.125232,],
-        {title: "Ohio Wildlife Center"})
-        .bindPopup('<strong>Home2 Suites').openPopup()
-        .addTo(map)
+
+
         
