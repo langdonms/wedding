5af2bcae322e996d71a145dfb7f82732d272ce7e
langdonms <langdon.sanders@gmail.com>
2018-06-11 21:33:45 -0400

    updated map style

--- js/mapleaflet.js
+++ js/mapleaflet.js
@@ -5,7 +5,7 @@
       )
         .setView([40.1764, -83.1397], 15);
         
-        L.tileLayer('https://api.mapbox.com/styles/v1/langdonms/cjddrtlfndyz22rkegavnupoi/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoibGFuZ2Rvbm1zIiwiYSI6ImNpZmQxdzE1dzRyd2RzbWx4bnp2Ym5maDcifQ.mrTKjuh7OcKwTDWc95OlZQ', {
+        L.tileLayer('https://api.mapbox.com/styles/v1/langdonms/cjhq6m9kq4rtu2st8ipatkg2q/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoibGFuZ2Rvbm1zIiwiYSI6ImNpZmQxdzE1dzRyd2RzbWx4bnp2Ym5maDcifQ.mrTKjuh7OcKwTDWc95OlZQ', {
             maxZoom: 18,
             minZoom: 3,
             attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
